f123f9aa9fcc2d5bc07b8f6d4a9157d693c125f6
kingsman142 <kingsman142@yahoo.com>
2016-09-23 16:06:19 -0400

    finished lab 3 for cs0447

--- Lab3/lab03part2.asm
+++ Lab3/lab03part2.asm
@@ -1,16 +1,22 @@
 .data
 	xyCalcMsg: .asciiz "x*y calculator"
-	enterXMsg: .asciiz "\nPlease enter x: "
-	enterYMsg: .asciiz "\nPlease enter y: "
+	enterXMsg: .asciiz "Please enter x: "
+	enterYMsg: .asciiz "Please enter y: "
 	asterisk : .asciiz "*"
 	equals   : .asciiz " = "
 	nonnegMsg: .asciiz "Integer must be nonnegative."
+	newLine  : .asciiz "\n"
 	
 .text
 	addi $v0, $0, 4
 	lui  $at, 0x00001001
 	ori  $a0, $at, 0x00000000
 	syscall 
+	
+	addi $v0, $0, 4
+	lui  $at, 0x00001001
+	ori  $a0, $at, 0x00000054
+	syscall
 	
 	promptForX:
 		addi $v0, $0, 4
@@ -24,27 +30,78 @@
 		slt  $t0, $v0, $0
 		beq  $t0, 1, integerNegativeErrorX
 		
+		addi $t1, $v0, 0
+		
 	promptForY:
 		addi $v0, $0, 4
 		lui  $at, 0x00001001
-		ori  $a0, $at, 0x000000021
+		ori  $a0, $at, 0x000000020
 		syscall
 		
 		addi $v0, $0, 5
 		syscall
 		slt  $t0, $v0, $0
 		beq  $t0, 1, integerNegativeErrorY
+		
+		addi $t2, $v0, 0
+		addi $t4, $v0, 0
+	
+	multiply:
+		beq  $t4, $0, end
+		add  $t3, $t3, $t1
+		sub  $t4, $t4, 1
+		j multiply
+	
+	j end
 	
 	integerNegativeErrorX:
 		addi $v0, $0, 4
 		lui  $at, 0x00001001
-		ori  $a0, $at, 0x00000039
+		ori  $a0, $at, 0x00000037
 		syscall
+		
+		addi $v0, $0, 4
+		lui  $at, 0x00001001
+		ori  $a0, $at, 0x00000054
+		syscall
+		
 		j promptForX
 		
 	integerNegativeErrorY:
 		addi $v0, $0, 4
 		lui  $at, 0x00001001
-		ori  $a0, $at, 0x00000039
+		ori  $a0, $at, 0x00000037
 		syscall
+		
+		addi $v0, $0, 4
+		lui  $at, 0x00001001
+		ori  $a0, $at, 0x00000054
+		syscall
+		
 		j promptForY
+		
+	end:
+		addi $v0, $0, 1
+		addi $a0, $t1, 0
+		syscall
+		
+		addi $v0, $0, 4
+		lui  $at, 0x00001001
+		ori  $a0, $at, 0x00000031
+		syscall
+		
+		addi $v0, $0, 1
+		addi $a0, $t2, 0
+		syscall
+		
+		addi $v0, $0, 4
+		lui  $at, 0x00001001
+		ori  $a0, $at, 0x00000033
+		syscall
+		
+		addi $v0, $0, 1
+		addi $a0, $t3, 0
+		syscall
+		
+		addi $v0, $0, 10
+		syscall
